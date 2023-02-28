459a18995ae0a8eb0a08703f875908bde256feb3
NikitaPups <nikita.zaga00@gmail.com>
2023-02-28 17:18:24 +0300

    Это наш первый комит

---
+++ L1/script.js
@@ -0,0 +1,26 @@
+'use strict';
+
+console.log(a);
+var a =1;
+
+
+var string ="hello";
+//var sym= Symbol();
+null; 
+undefined;
+//var obj = {};
+
+console.log(string)
+console.log(2/0)
+console.log('string'*9)
+ let persone={
+    name: "John",
+    age: 25,
+    IsMarried: false
+ }
+ console.log(persone["name"]);
+ let arr = ['plum.png', 'banana.jpg', 'pineapple.bmp'];
+ console.log(arr[2])
+ //alert ("Маша маленький пупс");
+let answer = prompt("are you ok?", "no");
+console.log(answer)
